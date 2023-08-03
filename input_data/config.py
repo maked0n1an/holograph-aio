ed2d9cac6c80d6c8a1ab79fd9aa68b9774666231
Alex <amedik733@gmail.com>
2023-08-03 12:41:26 +0300

    Updated config.py

--- input_data/config.py
+++ input_data/config.py
@@ -1,21 +1,22 @@
 from util.chain import Chain
 
-NFT_NAME = 'Green Skull'
-NFT_CONTRACT = '0x61B2d56645d697Ac3a27c2fa1e5B26B45429d1A9'
+NFT_NAME = 'Cosmic flower'
+NFT_CONTRACT = '0xd4Feff615c0E90f06340Be95d30e1f397779A184'
 
 MODE = 'minter'
 
-AMOUNT_OF_NFTS = (1, 2)
+AMOUNT_OF_NFTS = (1, 10)
 
 IS_SHUFFLE_KEYS = 1
 
-CHAINS_FOR_BRIDGE = [Chain.AVALANCHE, Chain.POLYGON, Chain.BSC, Chain.OPTIMISM]
+CHAINS_FOR_BRIDGE = [Chain.AVALANCHE, Chain.POLYGON, Chain.BSC, Chain.OPTIMISM, Chain.ARBITRUM]
 FEE_MULTIPLICATOR = 1.8
 
 # перерыв между действиями
-DELAY = (30, 100)
+DELAY = (200, 700)
 
 # moralis api key - https://admin.moralis.io/login идем сюда и получаем апи ключ, НУЖЕН DEFAULT KEY!, нужно для нахождения id нфт
 MORALIS_API_KEY = ''
+
 # cколько максимум секунд скрипт будет ждать подтверждения транзакции
 MAX_WAIT_TIME = 150
